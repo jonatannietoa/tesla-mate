13ece2906b787ae14cb4417218b2227b07c7c4bc
jonatansansaru <jonatan@sansarushop.com>
2020-06-04 00:17:24 +0200

    Script

--- script.sh
+++ script.sh
@@ -6,12 +6,4 @@ sudo usermod -a -G docker ec2-user
 sudo docker info
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
 git clone https://github.com/jonatannietoa/tesla-mate.git
-cd teslamate
-
-vim .env
-(Apretar a, editar y para guardar :wq)
-
-sudo service docker start
-docker-compose up
